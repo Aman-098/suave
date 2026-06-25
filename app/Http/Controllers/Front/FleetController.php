03b4a5132140b4823eea9bbc96caf384721a6b79
Aman Yadav <123934977+Aman-098@users.noreply.github.com>
2026-06-25 18:10:02 +0530

    price by asc

--- app/Http/Controllers/Front/FleetController.php
+++ app/Http/Controllers/Front/FleetController.php
@@ -15,6 +15,7 @@ class FleetController extends Controller
     public function index(){
         $fleets = Product::with('category')
                     ->where('status',1)
+                    ->orderBy('price', 'asc')
                     ->get()
                     ->groupBy('category.name');
         // dd($fleets); 

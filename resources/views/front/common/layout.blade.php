cf8e354ea7add8bc63b9839a33260f1875644ecf
Aman Yadav <123934977+Aman-098@users.noreply.github.com>
2026-07-11 12:37:28 +0530

    gtag

--- resources/views/front/common/layout.blade.php
+++ resources/views/front/common/layout.blade.php
@@ -2,6 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5948FNMC');</script>
+<!-- End Google Tag Manager -->
     <meta charset="utf-8">
 
     <title>SUAVE | Luxury Car Rental</title>
@@ -23,6 +30,10 @@
 </head>
 
 <body class="body counter-scroll">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5948FNMC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div class="preload preload-container">
         <div class="middle">
         </div>
